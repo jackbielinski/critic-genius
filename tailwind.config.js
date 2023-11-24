756b872f97642950d0b3ca394db00708644ec9cf
Jack Bielinski <jackebielinski@gmail.com>
2023-11-24 15:21:37 -0500

    first commit - this is basically just testing stuff and designing it

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./web/**/*.{html,js,php}"],
+  theme: {
+    extend: {
+      colors: {
+        "genius-yellow": "#ffff64",
+      },
+    },
+  },
+  plugins: [],
+}
